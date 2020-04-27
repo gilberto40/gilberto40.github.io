5cdf002a395fa5f357583856bf77a3273fc1b598
Gilberto <gilbertomaterano40@gmail.com>
2020-04-27 00:38:02 -0300

    se monto algo

---
+++ script.js
@@ -0,0 +1,13 @@
+let cel =document.querySelector("#cel")
+let mail=document.querySelector("#mail")
+cel.addEventListener("click",(e)=>{
+    e.preventDefault()
+    alert("11-2391-5579")
+    
+})
+
+mail.addEventListener("click",(e)=>{
+    e.preventDefault()
+    alert("gilbertomaterano40@gmail.com")
+    
+})
